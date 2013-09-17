574a3f76570a12740a4c545bd5ef4dcb700e2555
RonnyRusten <rrusten@mimer.no>
2013-09-17 15:34:12 +0200

    Initial

---
+++ frmMain.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace TikkTakk2013
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnSetup = new System.Windows.Forms.ToolStripButton();
+            this.btnStats = new System.Windows.Forms.ToolStripButton();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.tmrMain = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.lblTime = new System.Windows.Forms.Label();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnSetup,
+            this.btnStats});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(284, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnSetup
+            // 
+            this.btnSetup.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnSetup.Image = global::TikkTakk2013.Properties.Resources.Tool;
+            this.btnSetup.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnSetup.Name = "btnSetup";
+            this.btnSetup.Size = new System.Drawing.Size(23, 22);
+            this.btnSetup.Text = "toolStripButton1";
+            this.btnSetup.ToolTipText = "Innstillinger";
+            // 
+            // btnStats
+            // 
+            this.btnStats.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnStats.Image = global::TikkTakk2013.Properties.Resources.Table;
+            this.btnStats.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnStats.Name = "btnStats";
+            this.btnStats.Size = new System.Drawing.Size(23, 22);
+            this.btnStats.Text = "toolStripButton2";
+            this.btnStats.ToolTipText = "Sammendrag";
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Location = new System.Drawing.Point(12, 39);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(40, 13);
+            this.lblDate.TabIndex = 1;
+            this.lblDate.Text = "lblDate";
+            // 
+            // tmrMain
+            // 
+            this.tmrMain.Enabled = true;
+            this.tmrMain.Interval = 1000;
+            this.tmrMain.Tick += new System.EventHandler(this.tmrMain_Tick);
+            // 
+            // lblTime
+            // 
+            this.lblTime.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.Location = new System.Drawing.Point(187, 25);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(85, 31);
+            this.lblTime.TabIndex = 2;
+            this.lblTime.Text = "lblTime";
+            this.lblTime.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.toolStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.Text = "TikkTakk 2013";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnSetup;
+        private System.Windows.Forms.ToolStripButton btnStats;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Timer tmrMain;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label lblTime;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TikkTakk2013
+{
+    public partial class frmMain : Form
+    {
+        TikkTakk2013.clsCommon.clsPublic commonFunctions;
+        Boolean _SaveWindow;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            int Left = 0;
+            int Top = 0;
+            int Width = 0;
+            int Height = 0;
+            commonFunctions = new TikkTakk2013.clsCommon.clsPublic();
+            lblDate.Text = DateTime.Now.ToLongDateString();
+            lblTime.Text = DateTime.Now.ToLongTimeString();
+            _SaveWindow = commonFunctions.WindowSizePos(this.Name, ref Left, ref Top, ref Width, ref Height);
+        }
+
+        private void tmrMain_Tick(object sender, EventArgs e)
+        {
+            lblDate.Text = DateTime.Now.ToLongDateString();
+            lblTime.Text = DateTime.Now.ToLongTimeString();
+        }
+    }
+}
